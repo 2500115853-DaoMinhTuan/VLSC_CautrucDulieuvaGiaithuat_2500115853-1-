be87bb21869fb575d6568bd317fe218b4c7e14c5
2500115853-DaoMinhTuan <2500115853@vanlangsaigon.edu.vn>
2026-03-23 10:02:22 +0700

    Ađ DSA_Session03 and clean up folders

--- DSA_Session2/Program.cs
+++ DSA_Session2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static long GiaiThua(int n )
+    {
+        if (n <= 1) return 1;
+        return n * GiaiThua(n - 1);
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("{0}! = {1}", n, GiaiThua(n));
+        Console.WriteLine("Fibonacci({0}) = {1}", n, Fibonacci(n));
+    }
+}
